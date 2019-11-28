b3134984249a1bcf06db517ca2d57ae081be49af
root <root@ip-172-31-34-143.us-east-2.compute.internal>
2019-11-28 14:43:56 +0000

    code

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.coghw13fheqo.us-east-2.rds.amazonaws.com"
+customuser = "root"
+custompass = "root1234"
+customdb = "employee"
+custombucket = "addemp-1"
+customregion = "us-east-2"
+
